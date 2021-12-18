354b6c09bcab8758cc7a45a38f585b5c04ebe0a4
tonytrinh19 <tony.q.trinh@gmail.com>
2021-12-17 21:01:04 -0800

    Reorder folders. Changes html to ejs files. Adding routes for backend.

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const mongoose = require('mongoose')
-
 const config = require('config');
 const dbConfig = config.get('BeautifulSoup.dbConfig.db')
+const port = process.env.PORT || 3000
+const viewsPath = path.join(__dirname, './views')
+
+const imagesDirectory = path.join(__dirname, '/images')
+const publicDirectory = path.join(__dirname, '/public')
+
+app.use(express.static(imagesDirectory))
+app.use(express.static(publicDirectory))
+
+app.use(express.json())
+app.set('view engine', 'ejs')
+app.set('views', viewsPath)
 
 mongoose.connect(dbConfig, {
     useNewUrlParser: true,
@@ -14,4 +26,17 @@ mongoose.connect(dbConfig, {
     console.log('Unable to connect to DB' + err)
 })
 
+app.get('/main', (req,res) => {
+    res.render('main', {
 
+    })
+})
+
+app.get('/catalog', (req,res) => {
+    res.render('catalog', {
+
+    })
+})
+
+
+app.listen(port, () => console.log('Server is up on port ', port))
