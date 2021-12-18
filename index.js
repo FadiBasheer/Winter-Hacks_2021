a14bf5b98bccf0c8b0f1cf3fdc4fae72c4bc7cff
tonytrinh19 <tony.q.trinh@gmail.com>
2021-12-17 22:52:42 -0800

    Moved mongoose to a seperate folder.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const mongoose = require('mongoose')
-const config = require('config');
-const dbConfig = config.get('BeautifulSoup.dbConfig.db')
+
+require('./db/mongoose.js')
+
 const port = process.env.PORT || 3000
 const viewsPath = path.join(__dirname, './views')
 
@@ -17,27 +17,20 @@ app.use(express.json())
 app.set('view engine', 'ejs')
 app.set('views', viewsPath)
 
-mongoose.connect(dbConfig, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Db connected')
-}).catch(err => {
-    console.log('Unable to connect to DB' + err)
-})
 
+//Main Page
 app.get('/main', (req,res) => {
     res.render('main', {
 
     })
 })
 
+//Catalog Page
 app.get('/catalog', (req,res) => {
     res.render('catalog', {
 
     })
 })
-
 
 app.get('/*', (req,res) => {
     res.render('error', {
--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
-const Task = require('./task')
 
 const userSchema = mongoose.Schema({
     name: {
